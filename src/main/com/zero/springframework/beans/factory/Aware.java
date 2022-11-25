9fbbf077be4621d17aecf2bf9aa0eb2f1ffbe0c1
zero <190998853@qq.com>
2022-11-25 14:43:15 +0800

    添加初感知接口相关注释

--- src/main/com/zero/springframework/beans/factory/Aware.java
+++ src/main/com/zero/springframework/beans/factory/Aware.java
@@ -2,7 +2,7 @@ package com.zero.springframework.beans.factory;
 
 /**
  * @author zero
- * @description Aware
+ * @description 标记类接口，实现改接口后可以被Spring Bean容器感知
  * @date 2022/6/1 14:23
  */
 public interface Aware {
